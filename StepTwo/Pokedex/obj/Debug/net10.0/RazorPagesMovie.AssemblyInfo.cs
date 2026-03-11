88f4190725a32de29d10e775e36e01e81599053e
SadieLewis <you@example.com>
2026-03-11 15:57:57 -0500

    More style adjustments

--- StepTwo/Pokedex/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
+++ StepTwo/Pokedex/obj/Debug/net10.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+202bfb498a67495013c619403ad55386c7782720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed0b3d1a628dbb1a3cb150fd47d801ed7da8ea4")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
